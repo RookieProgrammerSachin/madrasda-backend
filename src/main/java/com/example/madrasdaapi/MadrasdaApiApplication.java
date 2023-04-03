92c846ca72e018ed94cc0135967531d33fa208e4
majestymewtwo <majestymewtwo@gmail.com>
2023-04-03 20:58:00 +0530

    made corrections to vendorMapper and AdminController

--- src/main/java/com/example/madrasdaapi/MadrasdaApiApplication.java
+++ src/main/java/com/example/madrasdaapi/MadrasdaApiApplication.java
@@ -3,11 +3,9 @@ package com.example.madrasdaapi;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@RefreshScope
 public class MadrasdaApiApplication {
 
      public static void main(String[] args) {
